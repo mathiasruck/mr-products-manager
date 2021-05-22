c4e17b0517271325abe2a591c7745de991ed26c4
Mathias Ruckert <mruckert@segware.com>
2021-05-22 11:05:45 -0300

    Starting to configure exception

---
+++ src/main/java/com/mathiasruck/mrproductsmanager/exception/ErrorResponse.java
@@ -0,0 +1,13 @@
+package com.mathiasruck.mrproductsmanager.exception;
+
+public class ErrorResponse {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
---
+++ src/main/java/com/mathiasruck/mrproductsmanager/exception/NoRecordFoundException.java
@@ -0,0 +1,10 @@
+package com.mathiasruck.mrproductsmanager.exception;
+
+public class NoRecordFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public NoRecordFoundException() {
+        super();
+    }
+}
