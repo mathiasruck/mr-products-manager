d209c2c34713a109a10cfcf343f5541b51766c16
Mathias Ruckert <mruckert@segware.com>
2021-05-25 20:01:33 -0300

    Removing unnecessary code

--- src/main/java/com/mathiasruck/mrproductsmanager/controller/AuthenticationController.java
+++ src/main/java/com/mathiasruck/mrproductsmanager/controller/AuthenticationController.java
@@ -7,7 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mathiasruck.mrproductsmanager.dto.AuthenticationRequestDto;
@@ -15,7 +14,6 @@ import com.mathiasruck.mrproductsmanager.model.AuthenticationResponse;
 import com.mathiasruck.mrproductsmanager.service.AuthenticationService;
 
 @RestController
-@RequestMapping
 public class AuthenticationController {
     @Autowired
     private AuthenticationService authenticationService;
--- src/main/java/com/mathiasruck/mrproductsmanager/model/AuthenticationRequest.java
+++ src/main/java/com/mathiasruck/mrproductsmanager/model/AuthenticationRequest.java
@@ -9,16 +9,6 @@ public class AuthenticationRequest implements Serializable {
     private String username;
     private String password;
 
-    // need default constructor for JSON Parsing
-    public AuthenticationRequest() {
-
-    }
-
-    public AuthenticationRequest(String username, String password) {
-        this.setUsername(username);
-        this.setPassword(password);
-    }
-
     public String getUsername() {
         return username;
     }
